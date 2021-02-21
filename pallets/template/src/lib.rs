0b7398acb8da225ba832285c44166ee06519046b
chabashilah <doumodoumo@gmail.com>
2021-02-21 17:02:03 +0900

    Added basic proof of existence code

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -4,8 +4,10 @@
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// https://substrate.dev/docs/en/knowledgebase/runtime/frame
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
-use frame_system::ensure_signed;
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, ensure, traits::Get};
+use frame_system::{self as system, ensure_signed};
+
+use sp_std::prelude::Vec;
 
 #[cfg(test)]
 mod mock;
@@ -29,6 +31,7 @@ decl_storage! {
 		// Learn more about declaring storage items:
 		// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
 		Something get(fn something): Option<u32>;
+		Proofs get(fn proofs): map hasher(blake2_128_concat) Vec<u8> => (T::AccountId, T::BlockNumber);
 	}
 }
 
@@ -39,6 +42,8 @@ decl_event!(
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
 		SomethingStored(u32, AccountId),
+		ClaimCreated(AccountId, Vec<u8>),
+		ClaimRevoked(AccountId, Vec<u8>),
 	}
 );
 
@@ -99,5 +104,52 @@ decl_module! {
 				},
 			}
 		}
+
+		/// Allow a user to claim ownership of an unclaimed proof
+		#[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]
+		fn create_claim(origin, proof: Vec<u8>) {
+			// Verify that the incoming transaction is signed and store who the
+			// caller of this function is.
+			let sender = ensure_signed(origin)?;
+
+			
+
+			// Verify that the specified proof has not been claimed yet or error with the message
+			ensure!(!Proofs::<T>::contains_key(&proof), "This proof has already been claimed.");
+
+			// Call the `system` runtime module to get the current block number
+			let current_block = <system::Module<T>>::block_number();
+
+			// Store the proof with the sender and the current block number
+			Proofs::<T>::insert(&proof, (sender.clone(), current_block));
+
+			// Emit an event that the claim was created
+			Self::deposit_event(RawEvent::ClaimCreated(sender, proof));
+		}
+
+
+        /// Allow the owner to revoke their claim
+		#[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]		
+        fn revoke_claim(origin, proof: Vec<u8>) {
+            // Determine who is calling the function
+            let sender = ensure_signed(origin)?;
+
+            // Verify that the specified proof has been claimed
+			
+
+            ensure!(Proofs::<T>::contains_key(&proof), "This proof has not been stored yet.");
+
+            // Get owner of the claim
+            let (owner, _) = Proofs::<T>::get(&proof);
+
+            // Verify that sender of the current call is the claim owner
+            ensure!(sender == owner, "You must own this claim to revoke it.");
+
+            // Remove claim from storage
+            Proofs::<T>::remove(&proof);
+
+            // Emit an event that the claim was erased
+            Self::deposit_event(RawEvent::ClaimRevoked(sender, proof));
+        }		
 	}
 }
